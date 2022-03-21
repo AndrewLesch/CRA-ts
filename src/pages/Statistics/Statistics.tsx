e9523fbe88926e1380da2046bb0c04a9bc4991e0
Your Name <Andreykabreamz@gmail.com>
2022-03-21 15:55:09 +0300

    prehosting commit

--- src/pages/Statistics/Statistics.tsx
+++ src/pages/Statistics/Statistics.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Statistics: React.FC = () => {
   return (
     <div>
-      <h1>Статистика</h1>
+      <h1>Опциональная страничка (React Router test)</h1>
     </div>
   );
 };
